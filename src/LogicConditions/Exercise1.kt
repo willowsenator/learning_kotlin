65497159f4edd0b83ed246cf121cd0d66eb6997c
willowsenator <willowsenator@gmail.com>
2022-01-10 19:03:03 +0100

    Logic Conditions exercise 1

---
+++ src/LogicConditions/Exercise1.kt
@@ -0,0 +1,29 @@
+package LogicConditions
+
+fun main(){
+    print("Enter a number: ")
+    var num1 = readLine()?.toDouble()
+
+    print("Enter another number: ")
+    var num2 = readLine()?.toDouble()
+
+    if(num1!! > num2!!){
+        println("Sum $num1 + $num2: " + num1.plus(num2))
+        println("Substract $num1 - $num2: " + num1.minus(num2))
+    }
+    else{
+        println("Product $num1 * $num2: " + num1.times(num2))
+        println("Divide $num1 / $num2: " + num1.div(num2))
+    }
+
+    var result = when(num1 > num2){
+        true->{
+            "Sum $num1 + $num2: " + num1.plus(num2) + "\n"+ "Substract $num1 - $num2: " + num1.minus(num2)
+        }
+        false->{
+            "Product $num1 * $num2: " + num1.times(num2) + "\n" +"Divide $num1 / $num2: " + num1.div(num2)
+        }
+    }
+
+    print(result)
+}
