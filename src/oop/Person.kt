df16e5942245fbe6a7f00ce541066d8239e07738
willowsenator <willowsenator@gmail.com>
2022-06-05 23:13:26 +0200

    rename package

---
+++ src/oop/Person.kt
@@ -0,0 +1,32 @@
+package oop
+
+class Person(){
+    private var name:String? = null
+    private var age:Int? = null
+    private var genre:String? = null
+
+
+    constructor(name:String?, age:Int?, genre:String?):this(){
+        this.name = name;
+        this.age = age;
+        this.genre = genre;
+    }
+
+    fun getName(): String? {
+        return name;
+    }
+
+    fun getAge():Int?{
+        return age;
+    }
+
+    fun getGenre():String?{
+        return genre;
+    }
+
+    override fun toString(): String {
+        return "Person(name=$name, age=$age, genre=$genre)"
+    }
+
+
+}
---
+++ src/oop/main.kt
@@ -0,0 +1,8 @@
+package oop
+
+fun main(){
+    var person = Person("Omar", 40, "Hombre");
+    var person2 = Person();
+    println(person);
+    println(person2);
+}
