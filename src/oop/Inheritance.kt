5836c72de37c506d5aad5a9b9e27ee31e92fab3b
willowsenator <willowsenator@gmail.com>
2022-07-07 22:24:28 +0200

    Inheritance

---
+++ src/oop/Inheritance.kt
@@ -0,0 +1,28 @@
+package oop
+
+class Human {
+    fun greetings(){
+        println("Hi everybody")
+    }
+}
+
+open class Dog{
+    fun bark(){
+        println("Guauuu Guauuu")
+    }
+}
+
+class Spaniel : Dog(){
+    fun draggingEars(){
+        println("Dragging ears")
+    }
+}
+
+fun main() {
+    val spaniel = Spaniel()
+    spaniel.bark()
+    spaniel.draggingEars()
+
+    val human = Human()
+    human.greetings()
+}
